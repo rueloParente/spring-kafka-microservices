feac2c2cb961cfb1e589c43c3b2dd868ae787846
rueloparente <rueloparentedev@gmail.com>
2024-08-12 18:10:30 +0100

    #2 ADD value object BookID

---
+++ library-service/src/main/java/com/rueloparente/library_service/domain/value_object/BookID.java
@@ -0,0 +1,28 @@
+package com.rueloparente.library_service.domain.value_object;
+
+import com.rueloparente.library_service.ddd.DomainID;
+import java.util.Objects;
+
+public class BookID implements DomainID {
+    private final int value;
+
+    public BookID(int value) {
+       this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof BookID bookID)) return false;
+        return Objects.equals(value, bookID.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(value);
+    }
+}
---
+++ library-service/src/test/java/com/rueloparente/library_service/domain/book/BookIDTest.java
@@ -0,0 +1,78 @@
+package com.rueloparente.library_service.domain.book;
+
+import com.rueloparente.library_service.domain.value_object.BookID;
+import org.junit.jupiter.api.Test;
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookIDTest {
+
+    @Test
+    void shouldReturnBookIDWhenGivenValidValue() {
+        //Arrange
+        int value = 1;
+        BookID bookID = new BookID(value);
+
+        //Act
+        int result = bookID.getValue();
+        //Assert
+        assertEquals(value, result);
+    }
+
+    @Test
+    void shouldReturnTrueWhenGivenSameBookID() {
+        //Arrange
+        int value = 1;
+        BookID bookID1 = new BookID(value);
+        BookID bookID2 = new BookID(value);
+
+        //Act
+        boolean result = bookID1.equals(bookID2);
+
+        //Assert
+        assertTrue(result);
+    }
+
+    @Test
+    void shouldReturnFalseWhenGivenDifferentBookID() {
+        //Arrange
+        int value1 = 1;
+        int value2 = 2;
+        BookID bookID1 = new BookID(value1);
+        BookID bookID2 = new BookID(value2);
+
+        //Act
+        boolean result = bookID1.equals(bookID2);
+
+        //Assert
+        assertFalse(result);
+    }
+
+    @Test
+    void shouldReturnFalseWhenGivenDifferentObject() {
+        //Arrange
+        int value = 1;
+        BookID bookID = new BookID(value);
+
+        //Act
+        boolean result = bookID.equals(new Object());
+
+        //Assert
+        assertFalse(result);
+    }
+
+    @Test
+    void shouldReturnHashCodeWhenGivenValidValue() {
+        //Arrange
+        int value = 1;
+        BookID bookID = new BookID(value);
+
+        //Act
+        int result = bookID.hashCode();
+
+        //Assert
+        assertEquals(Objects.hashCode(value), result);
+    }
+
+}
