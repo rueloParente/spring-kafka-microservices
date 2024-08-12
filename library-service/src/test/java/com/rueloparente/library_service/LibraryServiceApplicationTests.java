fcb9fc3946164b40ef76025622d287ae21453724
rueloparente <rueloparentedev@gmail.com>
2024-08-12 15:26:49 +0100

    closes #1, ADD skeleton for library-service. UPDATE root pom with new module

---
+++ library-service/src/test/java/com/rueloparente/library_service/LibraryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rueloparente.library_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
