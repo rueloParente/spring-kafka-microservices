6d6c6fbe33cf8ada25b28248077354e6791f3d8d
rueloparente <rueloparentedev@gmail.com>
2024-08-22 16:43:56 +0100

    start reservation-service, first commit

---
+++ reservation-service/src/main/java/com/rueloparente/reservation_service/ReservationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rueloparente.reservation_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationServiceApplication.class, args);
+	}
+
+}
